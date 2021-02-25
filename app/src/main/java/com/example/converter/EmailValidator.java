059531c821511015e7c5663110b385d54adc4e00
Krasav4ik <zkonst12@gmail.com>
2021-02-25 21:57:55 +0300

    Changed domains  in validator(ru,com,net)

--- app/src/main/java/com/example/converter/EmailValidator.java
+++ app/src/main/java/com/example/converter/EmailValidator.java
@@ -10,7 +10,7 @@ public class EmailValidator {
 
     private static final String EMAIL_PATTERN =
             "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
-                    "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+                    "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.(com|ru|net))$";
 
     public EmailValidator() {
         pattern = Pattern.compile(EMAIL_PATTERN);
