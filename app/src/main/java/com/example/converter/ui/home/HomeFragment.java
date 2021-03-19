5281a9af53bab4a18ee2409281e3f19526132c82
VadimKa <vadimmm120@yandex.ru>
2021-03-19 13:56:49 +0300

    Fragment display

--- app/src/main/java/com/example/converter/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/converter/ui/home/HomeFragment.java
@@ -1,6 +1,28 @@
 package com.example.converter.ui.home;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.converter.R;
 
 public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentLayout = inflater.inflate(R.layout.fragment_home, container, false);
+        NavController navController = NavHostFragment.findNavController(this);
+
+
+
+        return fragmentLayout;
+    }
 }
--- app/src/main/java/com/example/converter/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/converter/ui/profile/ProfileFragment.java
@@ -1,6 +1,27 @@
 package com.example.converter.ui.profile;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.converter.R;
 
 public class ProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentLayout = inflater.inflate(R.layout.fragment_profile, container, false);
+        NavController navController = NavHostFragment.findNavController(this);
+
+
+
+        return fragmentLayout;
+    }
 }
